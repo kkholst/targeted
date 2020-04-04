a00eec40c5dbdc48de13e1058303e2f1c14cbdfe
Klaus Holst <klaus@holst.it>
2020-04-04 15:58:13 +0200

    rpath fixed

--- python-package/src/targeted/__about__.py
+++ python-package/src/targeted/__about__.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 __name__        = "targeted"
-__version__     = "0.0.9"
+__version__     = "0.0.16"
 __license__     = "Apache Software License"
 __description__ = "Targeted inference"
 __author__      = u"Klaus Kähler Holst"
