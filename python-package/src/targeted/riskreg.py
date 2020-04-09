14a5dcbd3d7f0feaeb1435a77be236922949154f
Klaus Holst <klaus@holst.it>
2020-04-09 15:34:48 +0200

    documentation

--- python-package/src/targeted/riskreg.py
+++ python-package/src/targeted/riskreg.py
@@ -141,18 +141,17 @@ def riskreg_mle(y, a, x2, *args, **kwargs):
 
     .. math:: X(e^{j\omega } ) = x(n)e^{ - j\omega n}
 
-    And even use a greek symbol like :math:`omega` inline.
+    And even use a greek symbol like :math:`\omega` inline.
 
     References
     ----------
     Cite the relevant literature, e.g. [1]_.  You may also cite these
     references in the notes section above.
 
-    .. [1] O. McNoleg, "The integration of GIS, remote sensing,
-       expert systems and adaptive co-kriging for environmental habitat
-       modelling of the Highland Haggis using object-oriented, fuzzy-logic
-       and neural-network techniques," Computers & Geosciences, vol. 22,
-       pp. 585-588, 1996.
+    .. [1] Richardson, T. S., Robins, J. M., & Wang, L. (2017). On modeling and
+       estimation for the relative risk and risk difference. Journal of the
+       American Statistical Association, 112(519),
+       1121–1130. http://dx.doi.org/10.1080/01621459.2016.1192546
     """
     one = np.matrix(np.repeat(1.0, len(y))).transpose()
     x1 = kwargs.get('x1', one)
