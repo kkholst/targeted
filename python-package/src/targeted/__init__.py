5cb516a646a1fa011bb74f009f2e45fb04486fdf
Klaus Holst <klaus@holst.it>
2020-04-09 12:52:32 +0200

    export version

--- python-package/src/targeted/__init__.py
+++ python-package/src/targeted/__init__.py
@@ -3,3 +3,4 @@ from .__utils__ import expit, iid, robustse, tcrossprod  # noqa: F401
 from .get_data import get_data  # noqa: F401
 from .__targeted_c__ import riskregmodel, ace_est, datatype  # noqa: F401
 from .riskreg import riskreg  # noqa: F401
+from .__about__ import __version__  # noqa: F401
