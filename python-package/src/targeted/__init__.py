e046420be2a2e13de0b7b63e1d5ee75ecae91ee8
Klaus Holst <klaus@holst.it>
2020-04-17 23:30:21 +0200

    np.matrix -> np.array

--- python-package/src/targeted/__init__.py
+++ python-package/src/targeted/__init__.py
@@ -3,8 +3,4 @@ from .__utils__ import expit, iid, robustse, tcrossprod  # noqa: F401
 from .data import *  # noqa: F401,F403
 from .riskreg import riskreg  # noqa: F401
 from .__about__ import __version__  # noqa: F401
-
-try:
-    from .__targeted_c__ import riskregmodel, ace_est, datatype   # noqa: F401,E999
-except ImportError:
-    pass
+from .__targeted_c__ import riskregmodel, ace_est, datatype   # noqa: F401,E999
