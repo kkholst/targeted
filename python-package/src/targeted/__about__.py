a54436f0251d52b2da53dd106e1f9caae5eecf7f
Klaus Holst <klaus@holst.it>
2020-04-22 11:51:52 +0200

    updated package description

--- python-package/src/targeted/__about__.py
+++ python-package/src/targeted/__about__.py
@@ -1,9 +1,23 @@
 # coding: utf-8
 
 __name__        = "targeted"
-__version__     = "0.0.25"
+__version__     = "0.0.26"
 __license__     = "Apache Software License"
-__description__ = "Targeted inference"
+__description__ = """
+**targeted** provides a number of methods for semi-parametric
+estimation.  The library also contains implementations of various
+parametric models (including different discrete choice models) and
+model diagnostics tools.
+
+The implemention currently includes
+- **Risk regression models* with binary exposure
+  (Richardson et al. (2017) <doi:10.1080/01621459.2016.1192546>)
+- **Augmented Inverse Probability Weighted** estimators for missing data and causal inference
+  (Bang and Robins (2005) <doi:10.1111/j.1541-0420.2005.00377.x>)
+- Model diagnostics based on **cumulative residuals** methods
+- Efficient weighted **Pooled Adjacent Violator Algorithms**
+- **Nested multinomial logit** models
+"""
 __author__      = u"Klaus Kähler Holst"
 __email__       = "klaus.holst@maersk.com"
 __url__         = "https://target.readthedocs.io/en/latest/"
