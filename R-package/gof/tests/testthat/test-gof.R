740fe98d3edeee8e23c655fbe09a8d35c1eb4895
Klaus Holst <klaus@holst.it>
2020-04-27 21:25:45 +0200

    gof unit-tests

--- R-package/gof/tests/testthat/test-gof.R
+++ R-package/gof/tests/testthat/test-gof.R
@@ -1,7 +1,38 @@
-context("gof")
+context("cumres")
 
-testthat::test_that("Cumulative residuals test", {
-    testthat::expect_true(TRUE)
+## Cumulative residuals test
+
+testthat::test_that("One predictor, small sample size", {
+    n <- 500
+    x <- runif(n)
+    y <- x + rnorm(n)
+    l <- lm(y ~ x)
+    g <- cumres(l)
+    cc <- coef(g)
+    testthat::expect_equal(dim(cc), c(2,2))
+    testthat::expect_true(!any(is.na(cc)))
 })
 
 
+testthat::test_that("Several predictor, large sample size", {
+    n <- 1e4
+    p <- 10
+    X <- matrix(rnorm(p*n), ncol=p)
+    y <- X %*% rep(1,p) + rnorm(n)
+    l <- lm(y ~ X)
+    g <- cumres(l)
+    cc <- coef(g)
+    testthat::expect_equal(dim(cc), c(p+1, 2))
+    testthat::expect_true(!any(is.na(cc)))
+
+    g0 <- cumres(l, subset=NULL)
+    cc0 <- coef(g0)
+    testthat::expect_equal(dim(cc0), c(p+1, 2))
+    testthat::expect_true(!any(is.na(cc0)))
+
+    r <- residuals(l)
+    yhat <- predict(l)
+    ord <- order(yhat)
+    W <- 1/sqrt(n)*cumsum(r[ord])
+    testthat::expect_equivalent(W,g$W[[1]])
+})
