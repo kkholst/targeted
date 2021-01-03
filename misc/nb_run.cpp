0be2344e50db763d1a1504d3c202b1cfc4f84957
Klaus Holst <klaus@holst.it>
2021-01-03 13:14:29 +0100

    Adding new 'run' targets

---
+++ misc/nb_run.cpp
@@ -0,0 +1,22 @@
+#include <armadillo>
+#include <iostream>
+#include <ostream>
+#include <target/nb.hpp>
+#include <target/utils.hpp>
+
+using namespace arma;
+using std::string;
+using std::cout;
+using std::endl;
+
+
+int main(int argc, char **argv) {
+  mat A(2, 2);
+  A.fill(1);
+  A(1, 1) = 2;
+  cout << target::BLUE << "Naive Bayes test\n\n";
+  std::cout << inv(A) << std::endl;
+  std::cout << target::COL_RESET;
+  return 0;
+}
+
