1a2d080b51d306fe6860a0555665c72e28b4ede4
Klaus Holst <klaus@holst.it>
2021-02-11 08:59:45 +0100

    makefile variables updated

--- config/config.mk
+++ config/config.mk
@@ -1,30 +1,26 @@
 # -*- mode: makefile; -*-
 
-UNAME:=$(shell uname -s)
-CONTAINER_RUNTIME=$(shell command -v podman 2> /dev/null || echo docker)
-DOXYGEN=doxygen
-OPEN=$(shell which xdg-open || which gnome-open || which open)
-PYTHON=/usr/bin/env python3
-PIPARG= # e.g., --user
-PIP=$(PYTHON) -m pip $(PIPARG)
-PYTEST=/usr/bin/env pytest
-R=/usr/bin/env R --no-save --no-restore
-GIT=git
-CXX=
-CMAKE=/usr/bin/env cmake
-SELF_DIR=$(dir $(lastword $(MAKEFILE_LIST)))
-GETVER=$(abspath $(SELF_DIR))/getrversion.py
-LINTER:=$(or $(LINTER), cclint)
-NINJA=$(shell command -v ninja 2> /dev/null)
-ifeq ($(NINJA), 0)
-	override NINJA=
-endif
-NINJA_BUILD_OPT=-v
-FINDEXEC=
-SED_NOBACKUP=
+UNAME := $(shell uname -s)
+CONTAINER_RUNTIME = $(shell command -v podman 2> /dev/null || echo docker)
+DOXYGEN = doxygen
+OPEN = $(shell which xdg-open || which gnome-open || which open)
+PYTHON = /usr/bin/env python3
+PIPARG = # e.g., --user
+PIP = $(PYTHON) -m pip $(PIPARG)
+PYTEST = /usr/bin/env pytest
+R = /usr/bin/env R --no-save --no-restore
+GIT = git
+CMAKE = /usr/bin/env cmake
+SELF_DIR = $(dir $(lastword $(MAKEFILE_LIST)))
+GETVER = $(abspath $(SELF_DIR))/getrversion.py
+LINTER = cclint
+NINJA := $(shell command -v ninja 2> /dev/null)
+NINJA_BUILD_OPT = -v
+FINDEXEC :=
+SED_NOBACKUP :=
 ifeq ($(UNAME),Darwin)
-  FINDEXEC+= -perm +111
-  SED_NOBACKUP=
+  FINDEXEC +=  -perm +111
+  SED_NOBACKUP = ""
 else
-  FINDEXEC+= -executable
+  FINDEXEC +=  -executable
 endif
