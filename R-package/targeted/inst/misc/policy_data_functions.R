4d760f36861d98999830b91bdf35450473960af9
Andreas Nordland <andreas.nordland@maersk.com>
2021-03-08 11:13:29 +0100

    Update policy_data_functions.R

--- R-package/targeted/inst/misc/policy_data_functions.R
+++ R-package/targeted/inst/misc/policy_data_functions.R
@@ -1,5 +1,3 @@
-data("policy_data")
-
 copy_policy_data <- function(object){
   
   object$mp <- copy(object$mp)
@@ -74,7 +72,44 @@ markov_stage_history <- function(object, stage){
   return(sh)
 }
 
+markov_history <- function(object)
+  UseMethod("markov_history")
+
+markov_history.policy_data <- function(object){
+  mp_X_colnames <- object$colnames$mp_X_colnames
+  
+  sh_names <- c("id", "stage", "A", mp_X_colnames)
+  sh <- object$mp[event == 0, ]
+  sh <- sh[, ..sh_names]
+  
+  # sh <- list(
+  #   ref = sh[, c("id", "stage")],
+  #   A = sh[, c("A")],
+  #   X = sh[, ..mp_X_colnames]
+  # )
+  
+  return(sh)
+}
+
+utility <- function(object)
+  UseMethod("utility")
+
+utility.policy_data <- function(object){
+  mp <- object$mp
+  os <- object$os
+  
+  u <- mp[, .(U_mp = sum(U)), id]
+  u <- os[u, ]
+  u <- u[, .(id = id, U = U_mp + U_os)]
+  
+  return(u)
+}
+
 # Checks
+# data("policy_data")
+# tmp <- utility(policy_data)
+# tmp <- policy_data$mp
+# tmp2 <- markov_stage_history(policy_data, stage = 3)
 # tmp <- partial(policy_data, K = 5)
 # all(abs(tmp[, . (U = sum(U)), id]$U - policy_data$mp[, .(U = sum(U)), id]$U) < 1e-10)
 
