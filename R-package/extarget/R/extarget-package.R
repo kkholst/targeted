9cfa3f774fe0e547ad07280321f652fe53af5d23
Klaus Holst <klaus@holst.it>
2021-10-25 13:43:45 +0200

    version bump

---
+++ R-package/extarget/R/extarget-package.R
@@ -0,0 +1,15 @@
+
+##' Targeted -- linking exampled
+##'
+##' Demonstration on how to link to the targeted package
+##'
+##' @name extarget-package
+##' @import Rcpp methods
+##' @useDynLib extarget, .registration=TRUE
+##' @aliases extarget-package extarget
+##' @docType package
+##' @author Klaus K. Holst Maintainer: <klaus@@holst.it>
+##' @keywords package
+##' @examples
+##'
+NULL
