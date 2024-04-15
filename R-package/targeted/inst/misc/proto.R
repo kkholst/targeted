39a9b511b2c7c8e874f161f0c77a0920e156d74f
Klaus Holst <klaus@holst.it>
2024-04-15 10:15:43 +0200

    Roxygen and code clean-up (lintr)

--- R-package/targeted/inst/misc/proto.R
+++ R-package/targeted/inst/misc/proto.R
@@ -5,8 +5,8 @@ targetMLE_proto <- function(y,x,x1,x2=x1,weights=rep(1,length(y)),type="rd") {
     beta <- rep(0,ncol(x2))
     theta <- c(alpha,beta)
     pa <- function(theta) {
-        alpha <- theta[seq(ncol(x1))]
-        beta <- theta[seq(ncol(x2))+ncol(x1)]
+        alpha <- theta[seq_len(ncol(x1))]
+        beta <- theta[seq_len(ncol(x2)) + ncol(x1)]
         op <- exp(x2%*%beta)
         if (type=="rd") {
             target <- tanh(x1%*%alpha)
@@ -29,33 +29,33 @@ targetMLE_proto <- function(y,x,x1,x2=x1,weights=rep(1,length(y)),type="rd") {
         target <- pp[,4]
         op <- pp[,5]
         p <- pp[,1]
-        if (type=="rd") {        
+        if (type=="rd") {
             s0 <- p0 * (1 - p0)
             s1 <- p1 * (1 - p1)
             dp0.rd <- -s0/(s0 + s1)
             dp0.rd <- (dp0.rd + x)*(1 - target^2)
-            d.alpha = apply(x1,2, function(x) x*dp0.rd )
-            dp0.op = s0 * s1/(s0 + s1)
-            d.beta <- apply(x2,2, function(x) x*dp0.op )
+            d.alpha <- apply(x1, 2, function(x) x*dp0.rd)
+            dp0.op <- s0 * s1/(s0 + s1)
+            d.beta <- apply(x2, 2, function(x) x*dp0.op)
         } else {
             s <- (1-p0) + (1-p1)
-            tmp  <-  1 - x + x*target;
+            tmp  <-  1 - x + x*target
             dp.rr <- -p0/p1 * p0*(1-p0) / s * tmp + x*p0
             dp.rr  <- dp.rr*target
             d.alpha <- apply(x1,2, function(x) x*dp.rr)
-            s <- (1-p1)*p1 + (1-p0)*p1            
-            dp.op  <- (1-p0)^2*(1-p1)^2 / s * tmp * op;
-            d.beta <- apply(x2,2, function(x) x*dp.op)            
+            s <- (1-p1)*p1 + (1-p0)*p1
+            dp.op  <- (1-p0)^2*(1-p1)^2 / s * tmp * op
+            d.beta <- apply(x2,2, function(x) x*dp.op)
         }
         S <- (y-p)/(p*(1-p))
-        val <- apply(cbind(d.alpha,d.beta),2,function(x) weights*x*S)
+        val <- apply(cbind(d.alpha,d.beta), 2, function(x) weights*x*S)
         return(colSums(val))
     }
     d2logl <- function(theta) deriv(dlogl,theta)
     op <- nlminb(theta, function(p) -logl(p), function(p) -dlogl(p))
     V <- -Inverse(d2logl(op$par))
-    est <- estimate(coef=op$par,vcov=V)
-    list(estimate=est,opt=op, coef=op$par)
+    est <- estimate(coef=op$par, vcov=V)
+    list(estimate=est, opt=op, coef=op$par)
 }
 
 
@@ -63,7 +63,7 @@ Us <- function(alpha, theta,
         y, x,
         x1, x2=x1, x3=x2,
         weights=rep(1, length(y)),
-        optimal=TRUE, type="rd", indiv=TRUE, ...) {    
+        optimal=TRUE, type="rd", indiv=TRUE, ...) {
     x1 <- cbind(x1)
     x2 <- cbind(x2)
     x3 <- cbind(x3)
@@ -88,10 +88,10 @@ Us <- function(alpha, theta,
             denom <- (1-pr)/(p0*(1-p0)) + pr/(p1*(1-p1))
             omega <- nom/denom / ( pr*p0*(1-p0) )
         } else {
-            nom <- pr*p1/(1-p1)         
+            nom <- pr*p1/(1-p1)
             # E(A pa/(1-Pa) |V) = pr*drho/[p1(1-p1)]
             denom <- (1-pr)*p0/(1-p0) + pr*p1/(1-p1)
-            omega <- nom/denom / ( pr*(1-p0) )            
+            omega <- nom/denom / ( pr*(1-p0) )
         }
     }
     if (type=="rd") {
@@ -110,7 +110,9 @@ Us <- function(alpha, theta,
     colSums(res)
 }
 
-target_proto <- function(y,x,x1,x2=x1,x3=x2,weights=rep(1,length(y)),optimal=TRUE,std.err=FALSE,type="rd",...) {
+target_proto <- function(y, x, x1, x2=x1, x3=x2,
+                         weights=rep(1, length(y)),
+                         optimal=TRUE, std.err=FALSE, type="rd", ...) {
     x1 <- cbind(x1)
     x2 <- cbind(x2)
     x3 <- cbind(x3)
@@ -139,7 +141,9 @@ target_proto <- function(y,x,x1,x2=x1,x3=x2,weights=rep(1,length(y)),optimal=TRU
         pp <- pa(theta0)
         print(theta0)
         print(head(pp))
-        p0 <- pp[,2]; p1 <- pp[,3]; target <- pp[,4]
+        p0 <- pp[, 2]
+        p1 <- pp[, 3]
+        target <- pp[, 4]
         if (type=="rd") {
             ## omega.opt = (1 - rd) * (1 + rd)/(p0 * (1 - p0) + rd * (1 - pr) * 
             ##                                  (1 - 2 * p0 - rd))
@@ -177,18 +181,28 @@ target_proto <- function(y,x,x1,x2=x1,x3=x2,weights=rep(1,length(y)),optimal=TRU
     u <- function(p) as.vector(colSums(U(p)))
     opt <- nlminb(alpha0,function(x) sum(u(x)^2),...)
     if (std.err) {
-        thetahat <- c(theta0,gamma);
+        thetahat <- c(theta0,gamma)
         alphahat <- opt$par
         alpha.index <- seq_along(opt$par)
         theta.index <- seq_along(theta0) + length(alpha.index)
-        gamma.index <- seq_along(gamma) + length(alpha.index) + length(theta.index)    
+        gamma.index <- seq_along(gamma) + length(alpha.index)
+          + length(theta.index)
         pp <- c(alphahat, thetahat)
-        UU <- function(p,indiv=FALSE) Us(alpha=p[seq_along(alphahat)], theta=p[seq_along(thetahat)+length(alphahat)], y=y, x=x, x1=x1,x2=x2,x3=x3,weights=weights,optimal=optimal, indiv=indiv)
+        UU <- function(p, indiv = FALSE) {
+          Us(
+            alpha = p[seq_along(alphahat)],
+            theta = p[seq_along(thetahat) + length(alphahat)],
+            y = y, x = x, x1 = x1, x2 = x2, x3 = x3,
+            weights = weights, optimal = optimal, indiv = indiv
+          )
+        }
         DU  <- deriv(UU,pp)
         U0 <- UU(pp,indiv=TRUE)
         iid.gamma <- iid(propmod)
         iid.theta <- iid(mle$estimate)
-        ii <- (U0 - iid.theta%*%t(DU[,theta.index]) - iid.gamma%*%t(DU[,gamma.index])) %*% t(solve(DU[,alpha.index]))
+        ii <- (U0 - iid.theta %*% t(DU[, theta.index]) -
+               iid.gamma %*% t(DU[, gamma.index])) %*%
+          t(solve(DU[, alpha.index]))
         V <- crossprod(ii)
     } else {
         V <- ii <- NULL
