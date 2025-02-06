98aef7a20f3bf6a2d54f7a51be6a82b5913a4710
Klaus Holst <klaus@holst.it>
2025-02-06 09:02:56 +0100

    wip

---
+++ R/tune.R
@@ -0,0 +1,92 @@
+##'
+##'
+##'
+##' @title Hyperparameter tuning
+##' @param model
+##' @param bounds
+##' @param data
+##' @param n_iter
+##' @param kappa
+##' @param init_points
+##' @param nfolds
+##' @param model.score
+##' @param ...
+##' @return list with
+##' @details
+##'
+##' @author Klaus Kähler Holst
+tune <- function(model, bounds, data,
+                 n_iter = 15,
+                 kappa = 2.576,
+                 init_points = 4,
+                 nfolds = 5,
+                 model.score = targeted::scoring,
+                 ...) {
+  obj <- function(...) {
+    cl <- rlang::call_match(defaults = TRUE)
+    cl[1] <- expression(model)
+    f <- eval(cl)
+    res <- targeted::cv(list(f),
+        model.score = model.score,
+        nfolds = nfolds,
+        data = data,
+        silent=TRUE)
+    list(Score = -coef(res)[1], Pred = 0)
+  }
+  bo.args <- c(list(
+    FUN = obj,
+    bounds = bounds,
+    init_points = init_points,
+    n_iter = n_iter,
+    kappa = kappa
+  ), list(...))
+  arg <- names(bounds)
+  idx.cat <- which(unlist(lapply(bounds, is.list)))
+  if (length(idx.cat) > 0) {
+    par.cat <- arg[idx.cat]
+  }
+  res <- c()
+  if (length(par.cat) == 0) {
+      op <- do.call(rBayesianOptimization::BayesianOptimization, bo.args)
+      res <- list(op)
+  } else {
+    par.cat.grid <- expand.grid(bounds[idx.cat])
+    bounds[idx.cat] <- NULL
+    bo.args$bounds <- bounds
+      colnames(par.cat.grid) <- par.cat
+      for (i in seq_len(nrow(par.cat.grid))) {
+        obj0 <- obj
+        formals(obj0)[par.cat] <- par.cat.grid[i, ]
+        bo.args$FUN <- obj0
+        op <- do.call(rBayesianOptimization::BayesianOptimization, bo.args)
+        for (j in seq_len(length(par.cat))) {
+          op$History[[par.cat[j]]] <-
+            par.cat.grid[i, j]
+        }
+        res <- c(res, list(op))
+      }
+  }
+  res <- Reduce(rbind, lapply(res, function(x) x$History)) |>
+    as.data.frame()
+  ii <- which.max(res$Value)
+  par.opt <- res[ii, arg, drop=TRUE]
+  mod <- do.call(model, par.opt)
+  mod$estimate(data)
+  return(list(
+    history = res,
+    model = mod,
+    value = -res$Value[ii],
+    best = par.opt)
+    )
+}
+
+
+tuner <- function(mod, bounds, data, ...) {
+  modwrap <- function(...) {
+    args <- list(...)
+    mod$args[names(args)] <- args
+    mod$description <- ""
+    return(mod)
+  }
+  tune(modwrap, bounds=bounds, data=data, ...)
+}
