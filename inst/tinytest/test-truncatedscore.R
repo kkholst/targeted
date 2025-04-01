2887bacc0eca9d9f9f356a194e69df7b1c234d2d
Klaus Holst <klaus@holst.it>
2025-04-01 12:03:48 +0200

    unit test surv

---
+++ inst/tinytest/test-truncatedscore.R
@@ -0,0 +1,20 @@
+library("tinytest")
+library("mets")
+
+data("truncatedscore")
+a <- estimate_truncatedscore(
+  data = truncatedscore,
+  mod.y = y ~ a,
+  mod.r = r ~ a,
+  mod.a = a ~ 1,
+  mod.event = mets::Event(time, status>0) ~ 1,
+  time = 2
+)
+
+# testing the survival probability estimates
+test_truncatedscore_survival <- function() {
+  s <- phreg(Event(time, status > 0) ~ strata(a), data = truncatedscore)
+  pr <- predict(s, times = 2, data.frame(a = 0:1), se = 0)$surv[, 1]
+  expect_equivalent(coef(a)[4:5], pr, tolerance=1e-3)
+}
+test_truncatedscore_survival()
