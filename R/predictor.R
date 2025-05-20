715b4c20ec77ded0a82b729ecec5b8f9816d465b
Benedikt Sommer <55534750+benesom@users.noreply.github.com>
2025-05-20 10:20:06 +0200

    develop(superlearner): improved documentation for `superlearner` and new `predict.superlearner` s3 method  (#70)
    * changing how superlearner assigns names to unnamed values in learner.list
    * changing argument names of superlearner from model.list to learners
    * predict.superlearner tests and documentation
    * move s3 methods for predictor_sl to superlearner
    * simple tests for superlearner s3 methods
    * document referenced s3 superlearner methods
    * tests that all learners have the same response variable

--- R/predictor.R
+++ R/predictor.R
@@ -235,33 +235,18 @@ predictor_svm <- function(formula,
   return(mod)
 }
 
+
+#' @description [learner] generator function for [superlearner]
 #' @export
 #' @inherit predictor
-#' @title Superlearner (stacked/ensemble learner)
-#' @description This function creates a predictor object (class [ml_model])
-#'   from a list of existing [ml_model] objects. When estimating this model a
-#'   stacked prediction will be created by weighting together the predictions
-#'   of each of the initial models. The weights are learned using
-#'   cross-validation.
-#' @param model.list (list) List of [ml_model] objects (i.e. [predictor_glm])
-#' @param nfolds Number of folds to use in cross validation
-#' @param meta.learner meta.learner function (default non-negative least
-#'   squares). Must be a function of the response (nx1 vector), `y`, and the
-#'   predictions (nxp matrix), `pred`.
-#' @param model.score model scoring method (see [ml_model])
-#' @param ... additional argument to `superlearner`
-#' @aliases predictor_sl superlearner
-#' @references Luedtke & van der Laan (2016) Super-Learning of an Optimal
-#'   Dynamic Treatment Rule, The International Journal of Biostatistics.
-#' @seealso ml_model predictor_glm predictor_xgboost
+#' @inheritParams superlearner
+#' @param ... Additional arguments to [superlearner]
 #' @examples
 #' sim1 <- function(n = 5e2) {
-#'    n <- 5e2
 #'    x1 <- rnorm(n, sd = 2)
 #'    x2 <- rnorm(n)
 #'    y <- x1 + cos(x1) + rnorm(n, sd = 0.5**.5)
-#'    d <- data.frame(y, x1, x2)
-#'    d
+#'    data.frame(y, x1, x2)
 #' }
 #' d <- sim1()
 #'
@@ -281,14 +266,14 @@ predictor_svm <- function(formula,
 #'           lwd = 4, col = lava::Col("darkblue", 0.3))
 #' }
 #' print(s)
-#' # weights(s)
-#' # score(s)
+#' # weights(s$fit)
+#' # score(s$fit)
 #'
 #' cvres <- summary(s, data = d, nfolds = 3, rep = 2)
 #' cvres
 #' # coef(cvres)
 #' # score(cvres)
-predictor_sl <- function(model.list,
+predictor_sl <- function(learners,
                          info = NULL,
                          nfolds = 5L,
                          meta.learner = metalearner_nnls,
@@ -298,49 +283,33 @@ predictor_sl <- function(model.list,
 
   if (is.null(info)) {
     info <- "superlearner\n"
-    nn <- names(model.list)
+    nn <- names(learners)
     for (i in seq_along(nn)) {
       info <- paste0(info, "\t", nn[i])
       if (i < length(nn)) info <- paste0(info, "\n")
     }
   }
   args <- c(learner.args, list(info = info))
-  estimate.args <- list(model.list = model.list, nfolds = nfolds,
+  estimate.args <- list(learners = learners, nfolds = nfolds,
     meta.learner = meta.learner, model.score = model.score
   )
   args$estimate.args <- c(estimate.args, list(...))
   args$estimate <- function(data, ...) superlearner(data = data, ...)
-  args$predict <- function(object, newdata, all.learners = FALSE, ...) {
-    pr <- lapply(object$fit, \(x) x$predict(newdata))
-    res <- Reduce(cbind, pr)
-    if (!is.null(names(model.list)) &&
-      length(model.list) == ncol(res)) {
-      colnames(res) <- names(model.list)
-    } else {
-      colnames(res) <- paste0("model", seq_len(length(model.list)))
-    }
-    if (!all.learners) {
-      res <- as.vector(res %*% object$weights)
-    }
-    return(res)
-  }
+  args$predict <- function(object, newdata, ...) predict(object, newdata, ...)
 
-  mod <- do.call(ml_model$new, args)
-  mod$update(model.list[[1]]$formula)
+  mod <- do.call(learner$new, args)
+
+  # duplicate check from superlearner to catch error during instantiation
+  # of a learner instead in the estimate method call
+  if (length(unique(lapply(learners, \(m) all.vars(m$formula)[[1]]))) > 1) {
+    stop("All learners must have the same response variable.")
+  }
+  mod$update(learners[[1]]$formula) # TODO: not clean but will be fixed later
+  # because it requires changes to the learner R6 class and the cv function
 
   attr(mod, "model.score") <- model.score
   class(mod) <- c("predictor_sl", class(mod))
   return(mod)
-}
-
-#' @export
-weights.predictor_sl <- function(object, ...) {
-  return(object$fit$weights)
-}
-
-#' @export
-score.predictor_sl <- function(x, ...) {
-  return(x$fit$model.score)
 }
 
 score_sl <- function(response,
@@ -359,7 +328,7 @@ score_sl <- function(response,
   names(res)[1] <- "sl"
   nn <- names(res)
   names(res) <- paste0(nam, nn)
-  w <- c(NA, weights(object))
+  w <- c(NA, weights(object$fit))
   names(w) <- paste0("weight.", nn)
   return(c(res, w))
 }
--- inst/tinytest/test_predictor.R
+++ inst/tinytest/test_predictor.R
@@ -6,8 +6,7 @@ sim1 <- function(n = 5e2) {
    lp <- x2*x1 + cos(x1)
    yb <- rbinom(n, 1, lava::expit(lp))
    y <-  lp + rnorm(n, sd = 0.5**.5)
-   d <- data.frame(y, yb, x1, x2)
-   d
+   return(data.frame(y, yb, x1, x2))
 }
 d <- sim1()
 
@@ -111,3 +110,27 @@ test_predictor_svm <- function() {
 }
 test_predictor_svm()
 
+test_predictor_sl <- function() {
+  # test with oracle model
+  lrs <- list(
+    mean = predictor_glm(y ~ 1),
+    glm = predictor_glm(y ~ x1 + x2 + cos(x1)) # oracle for sim1
+  )
+  lr <- predictor_sl(lrs, nfolds = 2)
+  lr$estimate(d)
+
+  expect_equal(lr$fit$weights, c(mean = 0, glm = 1))
+  # verifies that nfolds argument is passed on to superlearner
+  expect_equal(length(lr$fit$folds), 2)
+
+  # default behavior is to return predictions of ensemble model
+  expect_equal(length(lr$predict(newdata = sim1(5))), 5)
+
+  # predictions can also be returned for individual learners
+  expect_equal(dim(lr$predict(newdata = sim1(5), all.learners = TRUE)), c(5, 2))
+
+  # nfolds can be overwritten in estimate method call
+  lr$estimate(d, nfolds = 3)
+  expect_equal(length(lr$fit$folds), 3)
+}
+test_predictor_sl()
