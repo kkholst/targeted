03a81d2976a22d37eb0f559915fa09e716c4ad81
Benedikt Sommer <55534750+benesom@users.noreply.github.com>
2025-05-22 11:52:39 +0200

    develop: renaming `predictor_sl` into `learner_sl` (#78)
    In addition, moving `learner_sl` from `predictor.R` into `learner_sl.R` file

--- R/cv.R
+++ R/cv.R
@@ -41,7 +41,7 @@ cv <- function(object, ...) UseMethod("cv")
 #'           learner_glm(Sepal.Length~Species + Petal.Length))
 #' x <- cv(m, rep=10, data=iris)
 #' x
-#' @seealso [cv.predictor_sl]
+#' @seealso [cv.learner_sl]
 #' @aliases cv cv.default
 #' @export
 cv.default <- function(object,
@@ -295,103 +295,6 @@ cv.default <- function(object,
   )
   return(obj)
 }
-
-score_sl <- function(response,
-                     newdata,
-                     object,
-                     model.score,
-                     ...) {
-  pr.all <- object$predict(newdata, all.learners = TRUE)
-  pr <- object$predict(newdata)
-  risk.all <- apply(pr.all, 2, function(x) model.score(x, response))
-  risk <- cbind(rbind(model.score(response, pr))[1, ])
-  nam <- names(risk)
-  if (is.null(nam)) nam <- "score"
-  nam <- paste0(nam, ".")
-  risk <- cbind(risk, rbind(risk.all))
-  colnames(risk)[1] <- "sl"
-  nn <- colnames(risk)
-  names(risk) <- paste0(nam, nn)
-  w <- rbind(c(NA, weights(object$fit)))
-  rownames(w) <- "weight"
-  risk <- rbind(risk, w)
-  res <- c()
-  for (i in seq_len(nrow(risk))) {
-    x <- risk[i, ]
-    names(x) <- paste0(rownames(risk)[i], ".", colnames(risk), sep="")
-    res <- c(res, x)
-  }
-  return(res)
-}
-
-#' Cross-validation for [predictor_sl]
-#' @description Cross-validation estimation of the generalization error of the
-#'   super learner and each of the separate models in the ensemble. Both the
-#'   chosen model scoring metrics as well as the model weights of the stacked
-#'   ensemble.
-#' @param object (predictor_sl) Instantiated [predictor_sl] object.
-#' @export
-#' @inheritParams cv.default
-#' @examples
-#' sim1 <- function(n = 5e2) {
-#'    x1 <- rnorm(n, sd = 2)
-#'    x2 <- rnorm(n)
-#'    y <- x1 + cos(x1) + rnorm(n, sd = 0.5**.5)
-#'    data.frame(y, x1, x2)
-#' }
-#' sl <- predictor_sl(list(
-#'                    "mean" = learner_glm(y ~ 1),
-#'                    "glm" = learner_glm(y ~ x1),
-#'                    "glm2" = learner_glm(y ~ x1 + x2)
-#'                   ))
-#' cv(sl, data = sim1(), rep = 2)
-cv.predictor_sl <- function(object,
-                            data,
-                            nfolds = 5,
-                            rep = 1,
-                            model.score = scoring,
-                            ...) {
-  res <- cv(list("performance"=object),
-            data = data,
-            nfolds = nfolds, rep = rep,
-            model.score = function(...) score_sl(..., model.score = model.score)
-            )
-  nam <- dimnames(res$cv)
-  nam <- nam[[length(nam)]]
-  st <- strsplit(nam, "\\.")
-  type <- unlist(lapply(st, \(x) x[1])) |> unique() # metrics
-  n <- length(nam)/length(type) # number of models
-  nam <- gsub(paste0(type[1], "\\."), "", nam[seq_len(n)])
-
-  idx <- 1:n
-  cvs <- c()
-  for (i in seq_len(length(type))) {
-    score <- res$cv[, , , idx + (i-1)*n, drop=FALSE]
-    cvs <- abind::abind(cvs, score, along=3)
-  }
-  dimnames(cvs)[[4]] <- nam
-  dimnames(cvs)[[3]] <- type
-  cvs <- aperm(cvs, c(1, 2, 4, 3))
-  res$names <- nam
-  res$cv <- cvs
-  res$call <- NULL
-  class(res) <- c("cross_validated.predictor_sl", "cross_validated")
-  return(res)
-}
-
-#' @export
-print.cross_validated.predictor_sl <- function(x, digits=5, ...) {
-  res <- round(summary.cross_validated(x)*1e5, digits=0) / 1e5
-  cat("\n", x$fold, "-fold cross-validation", sep="")
-  if (x$rep > 1) cat(" with ", x$rep, " repetitions", sep="")
-  cat("\n")
-  p <- dim(res)[3]
-  for (i in seq_len(p)) {
-    cli::cli_h3(dimnames(res)[[3]][i])
-    print(res[, , i], na.print="-")
-  }
-}
-
 
 summary_cv <- function(x) {
   x0 <- na.omit(x)
--- inst/tinytest/test_predictor.R
+++ inst/tinytest/test_predictor.R
@@ -109,27 +109,3 @@ test_predictor_svm <- function() {
 }
 test_predictor_svm()
 
-test_predictor_sl <- function() {
-  # test with oracle model
-  lrs <- list(
-    mean = learner_glm(y ~ 1),
-    glm = learner_glm(y ~ x1 + x2 + cos(x1)) # oracle for sim1
-  )
-  lr <- predictor_sl(lrs, nfolds = 2)
-  lr$estimate(d)
-
-  expect_equal(lr$fit$weights, c(mean = 0, glm = 1))
-  # verifies that nfolds argument is passed on to superlearner
-  expect_equal(length(lr$fit$folds), 2)
-
-  # default behavior is to return predictions of ensemble model
-  expect_equal(length(lr$predict(newdata = sim1(5))), 5)
-
-  # predictions can also be returned for individual learners
-  expect_equal(dim(lr$predict(newdata = sim1(5), all.learners = TRUE)), c(5, 2))
-
-  # nfolds can be overwritten in estimate method call
-  lr$estimate(d, nfolds = 3)
-  expect_equal(length(lr$fit$folds), 3)
-}
-test_predictor_sl()
