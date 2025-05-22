7a439c153a4ee1a073bbc84de5dd326416fb880e
Klaus Holst <klaus@holst.it>
2025-05-22 12:11:29 +0200

    fix tests

--- inst/tinytest/test_learner_glmnet.R
+++ inst/tinytest/test_learner_glmnet.R
@@ -1,3 +1,16 @@
+set.seed(42)
+
+sim1 <- function(n = 5e2) {
+   x1 <- rnorm(n, sd = 2)
+   x2 <- rnorm(n)
+   lp <- x2*x1 + cos(x1)
+   yb <- rbinom(n, 1, lava::expit(lp))
+   y <-  lp + rnorm(n, sd = 0.5**.5)
+   return(data.frame(y, yb, x1, x2))
+}
+d <- sim1(1e4)
+
+
 test_learner_glmnet <- function() {
   lr <- learner_glmnet_cv(y ~ x1 + x2, nfolds = 3, alpha = 0)
   lr$estimate(d)
