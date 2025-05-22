3fd243343e5a22a8ee589a6514bfad50e1745c4b
Benedikt Sommer <benediktsommer92@gmail.com>
2025-05-22 10:55:07 +0200

    moving cv s3 method to learner_sl.R

--- R/learner_sl.R
+++ R/learner_sl.R
@@ -74,3 +74,99 @@ learner_sl <- function(learners,
   class(mod) <- c("learner_sl", class(mod))
   return(mod)
 }
+
+score_sl <- function(response,
+                     newdata,
+                     object,
+                     model.score,
+                     ...) {
+  pr.all <- object$predict(newdata, all.learners = TRUE)
+  pr <- object$predict(newdata)
+  risk.all <- apply(pr.all, 2, function(x) model.score(x, response))
+  risk <- cbind(rbind(model.score(response, pr))[1, ])
+  nam <- names(risk)
+  if (is.null(nam)) nam <- "score"
+  nam <- paste0(nam, ".")
+  risk <- cbind(risk, rbind(risk.all))
+  colnames(risk)[1] <- "sl"
+  nn <- colnames(risk)
+  names(risk) <- paste0(nam, nn)
+  w <- rbind(c(NA, weights(object$fit)))
+  rownames(w) <- "weight"
+  risk <- rbind(risk, w)
+  res <- c()
+  for (i in seq_len(nrow(risk))) {
+    x <- risk[i, ]
+    names(x) <- paste0(rownames(risk)[i], ".", colnames(risk), sep="")
+    res <- c(res, x)
+  }
+  return(res)
+}
+
+#' Cross-validation for [learner_sl]
+#' @description Cross-validation estimation of the generalization error of the
+#'   super learner and each of the separate models in the ensemble. Both the
+#'   chosen model scoring metrics as well as the model weights of the stacked
+#'   ensemble.
+#' @param object (learner_sl) Instantiated [learner_sl] object.
+#' @export
+#' @inheritParams cv.default
+#' @examples
+#' sim1 <- function(n = 5e2) {
+#'    x1 <- rnorm(n, sd = 2)
+#'    x2 <- rnorm(n)
+#'    y <- x1 + cos(x1) + rnorm(n, sd = 0.5**.5)
+#'    data.frame(y, x1, x2)
+#' }
+#' sl <- learner_sl(list(
+#'                    "mean" = learner_glm(y ~ 1),
+#'                    "glm" = learner_glm(y ~ x1),
+#'                    "glm2" = learner_glm(y ~ x1 + x2)
+#'                   ))
+#' cv(sl, data = sim1(), rep = 2)
+cv.learner_sl <- function(object,
+                            data,
+                            nfolds = 5,
+                            rep = 1,
+                            model.score = scoring,
+                            ...) {
+  res <- cv(list("performance"=object),
+            data = data,
+            nfolds = nfolds, rep = rep,
+            model.score = function(...) score_sl(..., model.score = model.score)
+            )
+  nam <- dimnames(res$cv)
+  nam <- nam[[length(nam)]]
+  st <- strsplit(nam, "\\.")
+  type <- unlist(lapply(st, \(x) x[1])) |> unique() # metrics
+  n <- length(nam)/length(type) # number of models
+  nam <- gsub(paste0(type[1], "\\."), "", nam[seq_len(n)])
+
+  idx <- 1:n
+  cvs <- c()
+  for (i in seq_len(length(type))) {
+    score <- res$cv[, , , idx + (i-1)*n, drop=FALSE]
+    cvs <- abind::abind(cvs, score, along=3)
+  }
+  dimnames(cvs)[[4]] <- nam
+  dimnames(cvs)[[3]] <- type
+  cvs <- aperm(cvs, c(1, 2, 4, 3))
+  res$names <- nam
+  res$cv <- cvs
+  res$call <- NULL
+  class(res) <- c("cross_validated.learner_sl", "cross_validated")
+  return(res)
+}
+
+#' @export
+print.cross_validated.learner_sl <- function(x, digits=5, ...) {
+  res <- round(summary.cross_validated(x)*1e5, digits=0) / 1e5
+  cat("\n", x$fold, "-fold cross-validation", sep="")
+  if (x$rep > 1) cat(" with ", x$rep, " repetitions", sep="")
+  cat("\n")
+  p <- dim(res)[3]
+  for (i in seq_len(p)) {
+    cli::cli_h3(dimnames(res)[[3]][i])
+    print(res[, , i], na.print="-")
+  }
+}
