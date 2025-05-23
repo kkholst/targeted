4f198bf91a430552c59c9e249004130d3a89d986
Benedikt Sommer <benediktsommer92@gmail.com>
2025-05-23 10:40:29 +0200

    tests + rox

--- R/learner_isoreg.R
+++ R/learner_isoreg.R
@@ -4,18 +4,31 @@
 #' @inherit learner_shared
 #' @inheritParams targeted::isoregw
 #' @examples
-# TODO: examples
+#' x <- runif(5e3, -5, 5)
+#' pr <- lava::expit(-1 + x)
+#' y <- rbinom(length(pr), 1, pr)
+#' d <- data.frame(y, x)
+#'
+#' lr <- learner_isoreg(y ~ x)
+#' lr$estimate(d)
+#' pr_iso <- lr$predict(d)
+#'
+#' if (interactive()) {
+#'   plot(pr ~ x, cex=0.3)
+#'   lines(sort(x), pr_iso[order(x)], col="red", type="s")
+#' }
 learner_isoreg <- function(formula,
-                             info = "targeted::isoregw",
-                             learner.args = NULL,
-                             ...) {
+                           info = "targeted::isoregw",
+                           learner.args = NULL,
+                           ...) {
   if (length(all.vars(formula)) != 2) {
     stop("learner_isoreg: expected one outcome and one predictor variable")
   }
   args <- c(learner.args, list(formula = formula, info = info))
+  args$estimate.args <- list(...)
 
-  args$estimate <- function(y, x, ...) return(isoregw(y = y, x = x, ...))
+  args$estimate <- function(y, x, ...) isoregw(y = y, x = x, ...)
   args$predict <- function(object, newdata, ...) return(object(newdata))
 
-  do.call(learner$new, args)
+  return(do.call(learner$new, args))
 }
