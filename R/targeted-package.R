c40a68d484e66cfc65cfbe503d895a78c627c8bb
Klaus Holst <klaus@holst.it>
2025-07-03 09:42:50 +0200

    roxygen

--- R/targeted-package.R
+++ R/targeted-package.R
@@ -6,7 +6,7 @@
 #' @importFrom graphics plot points abline lines
 #' @importFrom grDevices nclass.Sturges
 #' @importFrom lava IC getoutcome estimate Inverse na.pass0 score logit expit
-#'   parameter
+#'   parameter sim
 #' @importFrom stats approxfun as.formula update binomial deriv density glm.fit
 #'   lm.wfit lm.fit glm lm coef vcov model.frame model.matrix na.pass nlminb
 #'   predict dnorm quantile terms weighted.mean runif .getXlevels
