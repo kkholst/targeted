2f0aeb47d237aa263d68b9d1693e2c1253e1ddc2
Klaus Holst <klaus@holst.it>
2025-08-15 22:18:31 +0200

    updated expand.list and learner_expand_grid

--- R/expand.list.R
+++ R/expand.list.R
@@ -7,47 +7,31 @@
 #' @return list
 #' @author Klaus Kähler Holst
 #' @examples
-#' expand.list(x=2:4, z=c("a","b"))
+#' expand.list(x = 2:4, z = c("a", "b"))
 #' @export
 expand.list <- function(..., INPUT = NULL, envir = NULL) {
-  dots <- c(INPUT, list(...))
-  nam <- names(dots)
-  nulls <- c()
-  formulas <- c()
-  for (i in seq_along(dots)) {
-    if (is.null(dots[[i]])) {
-      dots[[i]] <- "NULL"
-      nulls <- c(nulls, i)
+  inp <- c(INPUT, list(...))
+  for (i in seq_along(inp)) {
+    if (is.null(inp[[i]])) {
+      inp[[i]] <- list(NULL)
     }
-    if (is.list(dots[[i]])) {
-      if (inherits(dots[[i]][[1]], "formula")) {
-        if (is.null(envir)) envir <- environment(dots[[i]][[1]])
-        dots[[i]] <- unlist(lapply(dots[[i]], deparse1))
-        formulas <- c(formulas, i)
-      }
-    } else if (inherits(dots[[i]], "formula")) {
-      if (is.null(envir)) envir <- environment(dots[[i]])
-      dots[[i]] <- deparse1(dots[[i]])
-      formulas <- c(formulas, i)
+    if (!is.vector(inp[[i]]) &&
+        !inherits(inp[[i]], "list")
+        ) {
+      inp[[i]] <- list(inp[[i]])
     }
   }
-
-  names(dots) <- nam
-  dat <- do.call(
-    expand.grid,
-    c(dots, list(KEEP.OUT.ATTRS = FALSE, stringsAsFactors = FALSE))
-  )
-
-  fun <- function(i) {
-    res <- as.list(dat[i, ])
-    if (length(nulls)>0) res[nulls] <- list(NULL)
-    if (length(formulas)>0) {
-      res[[formulas]] <- as.formula(res[[formulas]])
-      environment(res[[formulas]]) <- envir
+  nn <- lapply(lapply(inp, length), seq_len)
+  gr <- expand.grid(nn)
+  res <- c()
+  for (i in seq_len(nrow(gr))) {
+    idx <- unlist(gr[i, ])
+    r <- c()
+    for (j in seq_along(idx)) {
+      r <- c(r, list(inp[[j]][[idx[j]]]))
     }
-    return(res)
+    names(r) <- names(inp)
+    res <- c(res, list(r))
   }
-  res <- lapply(seq_len(NROW(dat)), fun)
-
-  return(structure(res, table=dat))
+  return(res)
 }
