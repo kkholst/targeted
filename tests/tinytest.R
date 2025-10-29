696f1476c58bdc4532596d2ee371fb7943ea6241
Klaus Holst <klaus@holst.it>
2025-10-29 23:30:16 +0100

    limit number of cpus/threads for unit tests

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -1,7 +1,7 @@
 
 if (requireNamespace("tinytest", quietly = TRUE)) {
 
-  future::plan("sequential")
+  ## future::plan("sequential")
   Sys.setenv("OMP_THREAD_LIMIT" = 2)
   options(Ncpus = 1)
   data.table::setDTthreads(1)
