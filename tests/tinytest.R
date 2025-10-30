e8dd681f3a6dce61bb295a92d22dbafa90d952f5
Klaus Holst <klaus@holst.it>
2025-10-30 13:54:01 +0100

    tinytest ncpus/nthreads =2

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -1,10 +1,6 @@
 
 if (requireNamespace("tinytest", quietly = TRUE)) {
-
-  ## future::plan("sequential")
-  Sys.setenv("OMP_THREAD_LIMIT" = 2)
   options(Ncpus = 1)
   data.table::setDTthreads(1)
-
   tinytest::test_package("targeted")
 }
