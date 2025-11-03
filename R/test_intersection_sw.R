7d71bb1dfeb859d89cda0a143413319e18e33a51
Klaus Holst <klaus@holst.it>
2025-11-03 13:45:09 +0100

    zmax test (one-sided)

--- R/test_intersection_sw.R
+++ R/test_intersection_sw.R
@@ -24,6 +24,27 @@ pval.marg <- function(thetahat, sigmahat, noninf = 0) {
   1 - pnorm(z, sd = 1)
 }
 
+test_proc_arg <- function(par, vcov, index = NULL, ...) {
+  if (inherits(par, "estimate")) {
+    vcov <- stats::vcov(par)
+    par <- stats::coef(par)
+  }
+  args <- list(...) |>
+    lapply(function(x) { rep(x, length.out=length(par))})
+  argnames <- names(args)
+  if (!is.null(index)) {}
+  res <- c(list(par = par, vcov = vcov), args)
+  if (!is.null(index)) {
+    if (length(par) < length(index)) stop("wrong `index`")
+    res$par <- res$par[index]
+    res$vcov <- res$vcov[index, index, drop = FALSE]
+    for (arg in argnames) {
+      res[[arg]] <- res[[arg]][index]
+    }
+  }
+  return(res)
+}
+
 #' @title Signed Wald intersection test
 #' @description Calculating test statistics and p-values for the signed Wald
 #'   intersection test given by \deqn{SW = \inf_{\theta \in \cap_{i=1}^n H_i}
@@ -40,58 +61,49 @@ pval.marg <- function(thetahat, sigmahat, noninf = 0) {
 #' @param index subset of parameters to test
 #' @export
 #' @author Klaus Kähler Holst, Christian Bressen Pipper
-#' @return list with Wald
+#' @return `htest` object
 #' @examples
 #' S <- matrix(c(1, 0.5, 0.5, 2), 2, 2)
 #' thetahat <- c(0.5, -0.2)
 #' test_intersection_sw(thetahat, S, nsim.null = 1e5)
 #' test_intersection_sw(thetahat, S, weights = NULL)
 #'
-#' \dontrun{ # only on 'lava' >= 1.8.2
+#' \dontrun{
+#' # only on 'lava' >= 1.8.2
 #' e <- estimate(coef = thetahat, vcov = S, labels = c("p1", "p2"))
 #' lava::closed_testing(e, test_intersection_sw, noninf = c(-0.1, -0.1)) |>
 #'   summary()
 #' }
 test_intersection_sw <- function(par,
                                  vcov,
-                                 noninf = NULL,
-                                 weights = NULL,
+                                 noninf = 0,
+                                 weights = 1,
                                  nsim.null = 1e4,
                                  index = NULL) {
-  if (inherits(par, "estimate")) {
-    vcov <- stats::vcov(par)
-    par <- stats::coef(par)
-  }
-  if (is.null(noninf)) {
-    noninf <- rep(0, length(par))
-  }
-  if (is.null(weights)) {
-    weights <- rep(1, length(par))
-  }
-  if (!is.null(index)) {
-    if (length(par) < length(index)) stop("wrong `index`")
-    par <- par[index]
-    vcov <- vcov[index, index, drop = FALSE]
-    noninf <- noninf[index]
-    weights <- weights[index]
-  }
-  weights <- weights / sum(weights)
+  obj <- test_proc_arg(
+    par = par, vcov = vcov,
+    noninf = noninf,
+    weights = weights,
+    index = index
+  )
+  par <- obj$par
+  vcov <- obj$vcov
+  noninf <- obj$noninf
+  weights <- with(obj, weights / sum(weights))
   z <- (par - noninf) / diag(matrix(vcov))**.5
   if (length(z) == 1) {
     signwald <- (z**2) * (z >= 0)
-    ## 1-pnorm(z)
+    # 1-pnorm(z)
     pval <- ifelse(z >= 0,
       0.5 * pchisq(signwald, 1, lower.tail = FALSE), 1
-      )
+    )
     return(
       structure(list(
         data.name = sprintf("H0: b <= %g", noninf[1]),
         statistic = c("Q" = unname(signwald)),
-        estimate = c("b"=unname(par)),
+        estimate = c("b" = unname(par)),
         parameter = NULL,
         method = "Signed Wald Test",
-        # null.value =,
-        # alternative = "one.sided",
         alternative = sprintf("HA: b > %g", noninf[1]),
         p.value = pval
       ), class = "htest")
@@ -103,19 +115,19 @@ test_intersection_sw <- function(par,
     zmax <- max(z[1], z[2])
     signwald.intersect <- ifelse(zmax >= 0 & zmin <= (corr * zmax), 1, 0) *
       zmax * zmax + ifelse(zmax >= 0 & zmin > (corr * zmax),
-      (zmax * zmax + zmin * zmin - 2 * corr * zmax * zmin) /
-      (1 - corr * corr),
-      0
+        (zmax * zmax + zmin * zmin - 2 * corr * zmax * zmin) /
+          (1 - corr * corr),
+        0
       )
     # critval.intersect <- q_fct(alpha, corr)
     pval.intersect <- ifelse(signwald.intersect > 0,
       prob_fct(signwald.intersect, 0, corr), 1
-      )
+    )
   } else { # simulation-based inference
     sw <- .signedwald(par, vcov,
       noninf = noninf,
       weights = weights, nsim_null = nsim.null
-      )
+    )
     signwald.intersect <- sw$test.statistic
     pval.intersect <- sw$pval
   }
@@ -124,12 +136,66 @@ test_intersection_sw <- function(par,
     data.name = sprintf(
       "\n%s: theta =< [%s]\nw = [%s]",
       "Intersection null hypothesis",
-      paste(noninf, collapse = ", "),  w
+      paste(noninf, collapse = ", "), w
     ),
     statistic = c("Q" = unname(signwald.intersect)),
     parameter = NULL,
     method = "Signed Wald Intersection Test",
     p.value = pval.intersect
   ), class = "htest")
+  return(test.int)
+}
+
+#' @title One-sided Zmax test
+#' @description Calculating test statistics and p-values for the
+#'   onesided Zmax / minP test.z
+#'
+#'  Given parameter estimates \eqn{(\widehat{\theta}_1, \ldots,
+#'  \widehat{\theta}_p)^\top} with approximate assymptotic covariance matrix
+#'  \eqn{\widehat{S}}, let \eqn{ Z_i = \frac{\widehat{\theta}_i -
+#'  \delta_i}{\operatorname{SE}(\widehat{\theta}_i)}} , where
+#'  \eqn{\operatorname{SE}(\widehat{\theta}_i) = \widehat{S}_{ii}}. The Zmax
+#'  test statistic is then \eqn{Z_{max} = \max \{Z_1,\ldots,Z_p\}}, and the
+#'  null-hypothesis is \eqn{H_0: \theta_i \leq \delta_i, i=1,\ldots,p} with
+#'  non-inferiority margin \eqn{\delta_i, i=1,\ldots,p}, for which the p-value
+#'  is calculated as \eqn{ 1 - \Phi_R(Z_{max}) } with correlation matrix \eqn{R
+#'  = \operatorname{diag}(S_{11}^{-0.5}, \ldots,
+#'  S_{pp}^{-0.5})S\operatorname{diag}(S_{11}^{-0.5}, \ldots, S_{pp}^{-0.5})}.
+#' @param par (numeric) parameter estimates or `estimate` object
+#' @param vcov (matrix) asymptotic variance estimate
+#' @param noninf (numeric) non-inferiority margins
+#' @param index subset of parameters to test
+#' @return `htest` object
+#' @export
+#' @author Christian Bressen Pipper, Klaus Kähler Holst
+test_zmax_onesided <- function(par,
+                               vcov,
+                               noninf = 0,
+                               index = NULL) {
+  obj <- test_proc_arg(
+    par = par,
+    vcov = vcov,
+    noninf = noninf,
+    index = index
+  )
+  par <- obj$par
+  vcov <- obj$vcov
+  noninf <- obj$noninf
+  z <- (par - noninf) / diag(vcov)^0.5
+  zmax <- max(z)
+  pval.zmax <- 1 -
+    mets::pmvn(upper = rep(zmax, length(z)),
+               sigma = cov2cor(vcov))[1]
+  test.int <- structure(
+    list(
+      data.name = sprintf(
+        "\n%s: theta =< [%s]",
+        "Intersection null hypothesis",
+        paste(noninf, collapse = ", ")
+      ), statistic = c(Q = unname(zmax)), parameter = NULL,
+      method = "Zmax/minP test", p.value = pval.zmax
+    ),
+    class = "htest"
+  )
   return(test.int)
 }
--- inst/tinytest/test_intersection_sw.R
+++ inst/tinytest/test_intersection_sw.R
@@ -37,9 +37,9 @@ test_intersection_sw2 <- function(thetahat,
                                   noninf = rep(0, length(thetahat))) {
   W <- diag(weights)
   p <- length(thetahat)
-  ## sqrtS <- pracma::sqrtm(sigmahat)
-  sqrt.sigma <- expm::sqrtm(sigmahat)
-  sqrt.sigma.inv <- expm::sqrtm(solve(sigmahat))
+  sqrtS <- pracma::sqrtm(sigmahat)
+  sqrt.sigma <- sqrtS$B #expm::sqrtm(sigmahat)
+  sqrt.sigma.inv <- sqrtS$Binv #expm::sqrtm(solve(sigmahat))
   uhat <- (sqrt.sigma.inv %*% W %*%
            matrix(thetahat - noninf, length(thetahat), 1))
   ## ||uhat-u||^2 = (uhat-u)^T(uhat -h) =>xo
@@ -81,8 +81,7 @@ set.seed(1)
 dat <- do.call(simdata, sim_args)
 e <- est(dat)
 
-
-test_intersection_test_sw <- function() {
+test_test_intersection_sw <- function() {
   # check random seed works
   set.seed(1)
   e1 <- test_intersection_sw(coef(e), vcov(e))
@@ -128,3 +127,22 @@ test_intersection_test_sw <- function() {
   e0 <- test_intersection_sw(-0.2, v)
   expect_equivalent(1, e0$p.value)
 }
+test_test_intersection_sw()
+
+test_test_zmax_onesided <- function() {
+  z <- coef(e) / diag(vcov(e))**.5
+  t1 <- test_zmax_onesided(e, index = 2)
+  expect_equivalent(z[2], t1$statistic)
+  expect_equivalent(1 - pnorm(z[2]), t1$p.value)
+  t1 <- test_zmax_onesided(e, index = 3)
+  expect_equivalent(z[3], t1$statistic)
+  expect_equivalent(1 - pnorm(z[3]), t1$p.value)
+
+  idx <- 2:3
+  zmax <- max(z[idx])
+  e2 <- subset(e, idx)
+  t2 <- test_zmax_onesided(e, index = idx)
+  p2 <- 1 - mets::pmvn(upper=c(zmax, zmax), sigma=cov2cor(vcov(e2)))
+  expect_equivalent(p2, t2$p.value)
+}
+test_test_zmax_onesided()
