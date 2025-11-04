314c3d319f3b8998bd9685244b770fa7a8391c66
Klaus Holst <klaus@holst.it>
2025-11-04 13:42:28 +0100

    norm weights

--- inst/tinytest/test_intersection_sw.R
+++ inst/tinytest/test_intersection_sw.R
@@ -92,7 +92,7 @@ test_test_intersection_sw <- function() {
 
   # equal weights
   e1 <- test_intersection_sw(coef(e), vcov(e))
-  e2 <- test_intersection_sw2(coef(e), vcov(e), weights = c(1,1,1))
+  e2 <- test_intersection_sw2(coef(e), vcov(e), weights = c(1/3,1/3,1/3))
   expect_true(abs(e2$statistic - e1$statistic) < 1e-3)
   expect_true(abs(e2$p.value - e1$p.value) < 0.01)
 
