093af4f163e72680a0a2a826444c4e3adc835612
Klaus Holst <klaus@holst.it>
2025-11-12 09:06:54 +0100

    fix

--- R/test_intersection_sw.R
+++ R/test_intersection_sw.R
@@ -151,11 +151,11 @@ test_intersection_sw <- function(par,
       control$dykstra.niter <- 500
     }
     sv <- svd(vcov)
-    if (any(sv$d) < 0) {
+    if (any(sv$d < 0)) {
       cli::cli_warn("`vcov` is not positive definite.")
       cli::cli_warn(sprintf("Smallest singular value: %f", min(sv)))
       cli::cli_warn("Setting negative singular values to zero.")
-      sv$d[which(sv$d) < 0] <- 0
+      sv$d[which(sv$d < 0))] <- 0
       vcov <- with(sv, u %*% diag(d) %*% t(v))
     }
     if (is.null(control$pinv.tol)) {
