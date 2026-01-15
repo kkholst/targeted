9d29d22b4d5583786da35fcd10805c91148d240b
Benedikt Sommer <55534750+benesom@users.noreply.github.com>
2026-01-15 21:01:14 +0100

    surv estimating functions (#7)
    * Create surv_estimating_functions.R
    
    Initial implementation of the treatment level survival risk estimating functions.
    
    * added survival_treatment_level_estimating_functions: type = "prob"
    
    * Update surv_estimating_functions.R
    
    * init bug fixes in cumhaz
    
    * bug fixes for coxph.null in cumhaz + testthat
    
    * Update test-cumhaz.R
    
    * updated cumhaz - transposed chf outcome
    
    Added rudimentary tests for each of the implemented models.
    
    * updated surv_estimating_functions to account for updates on cumhaz
    
    * Index bug in surv_estimating_functions
    
    * Create test-surv_estimating_functions.R
    
    * In surv_estimating_functions: fixed use of factor levels
    
    * added tests for surv_estimating_functions
    
    * init roxy: survival_treatment_estimating_functions
    
    * update cumhaz test
    
    * init estimating functions for the rmst
    
    * init intsurv3
    
    * cumhaz for survfit objects
    
    * fixed bug in cumhaz for coxph.null objects
    
    * update int_surv
    
    * Update cumhaz.R
    
    * H_constructor
    
    * H_constructor_rmst
    
    * bug fix H_constructor_rmst
    
    * bug fix int_surv
    
    * included sample option in H_constructor_rmst
    
    * develop: switch from `testthat` to `tinytest` for unit testing of R package
    
    * wip
    
    * adding packages used in test_cumhaz to description file
    
    * remove expect_no_error because test will fail if error occurs in underlying function
    
    * some tests in test_rcai.R seem to be missing
    
    * done
    
    * minor
    
    * adding suppressPackageStartupMessages
    
    * minor
    
    * minor
    
    * updating R package manuals
    
    * use ::: for internal functions
    
    * develop: removing `devtools::load_all` from test rule and use only `tinytest::test_package`
    
    This change ensures that unit testing for R behaves identical when using `make r_test` and `make r_check`. This has not been the case before since `devtools::load_all` allows testing package internal functions without using `:::`. However, testing package internal functions with `make r_check` will fail if `:::` is not used. Installing the package before running the unit tests ensures that edited package functions and tests files are tested. In addition, a draft of contributing guidelines are added.
    
    * export cumhaz and perform minor formatting to cumhaz.R
    
    * minor
    
    * all test run
    
    * reset against base branch
    
    * remove double hashes
    
    * archive
    
    * move edited files into repo root
    
    * clean-up code and documentation
    
    * lint
    
    * roxy
    
    ---------
    
    Co-authored-by: OANO (Andreas Nordland) <OANO@novonordisk.com>
    Co-authored-by: Klaus Holst <klaus@holst.it>
    Co-authored-by: Benedikt Sommer <bkts@novonordisk.com>

---
+++ R/model_wrappers.R
@@ -0,0 +1,89 @@
+ clean_survival_model <- function(model) {
+  if (inherits(model, what = "phreg")) {
+    model$call <- NULL
+  }
+
+  return(model)
+}
+
+##' @title Fit survival nuisance models
+##' @param data data.frame
+##' @param response Response formula (e.g., Surv(time, event) ~ A + W)
+##' @param censoring Censoring formula (e.g., Surv(time, event == 0) ~ A + W))
+##' @param response_call Model call for the response model (e.g. "mets::phreg")
+##' @param response_args Additional arguments passed to the response model
+##' @param censoring_call Similar to response_callb
+##' @param censoring_args Similar to response_args
+##' @return List with elements T_model and C_model
+##' @author Andreas Nordland, Klaus K. Holst
+fit_survival_models <- function(data,
+                                response,
+                                censoring,
+                                response_call = "phreg",
+                                response_args = list(),
+                                censoring_call = "phreg",
+                                censoring_args = list()) {
+  ## response time-to-event (T) model:
+  T_args <- c(
+    list(
+      formula = response,
+      data = data
+    ),
+    response_args
+  )
+  T_model <- do.call(what = response_call, T_args)
+  T_model <- clean_survival_model(T_model)
+
+  ## censoring (C) model:
+  C_args <- c(
+    list(
+      formula = censoring,
+      data = data
+    ),
+    censoring_args
+  )
+  C_model <- do.call(what = censoring_call, C_args)
+  C_model <- clean_survival_model(C_model)
+
+
+  out <- list(
+    response = response,
+    T_model = T_model,
+    C_model = C_model
+  )
+
+  return(out)
+}
+
+fit_treatment_model <- function(data,
+                                treatment) {
+  # if treatment is a formula, the default super learner is applied:
+  if (inherits(treatment, "formula")) {
+    treatment <- SL(treatment, family = binomial())
+  }
+
+  ## check if both levels are observed:
+  A <- treatment$response(data)
+  A_levels <- sort(unique(A))
+  if (length(A_levels) != 2) {
+    stop("Expected binary treatment variable.")
+  }
+  # name of the treatment variable:
+  A_var <- all.vars(update(formula(treatment), ~1))
+  # binary representation of the treatment variable:
+  A_value <- (A == A_levels[2]) * 1
+
+  ## overwriting the treatment variable with the binary representation:
+  data[, A_var] <- A_value
+
+  # fitting the treatment model:
+  treatment$estimate(data)
+
+  out <- list(
+    A_model = treatment,
+    A_var = A_var,
+    A_levels = A_levels
+  )
+
+  return(out)
+}
---
+++ R/surv_h_constructor.R
@@ -0,0 +1,102 @@
+## ## Hu: H(u|X_i, A_i) = E[I\{T_i > \tau\} | T_i \geq u, X_i, A_i] =
+## ## I\{u \leq \tau \} \frac{S(\tau|X_i, A_i)}{S(u|X_i, A_i)} +  I\{u > \tau \}
+H_constructor_surv <- function(T_model, tau, individual_time, ...) {
+  force(T_model)
+  force(tau)
+  force(individual_time)
+
+  H <- function(u, data) {
+    S <- cumhaz(
+      T_model,
+      newdata = data,
+      times = u,
+      individual.time = individual_time
+    )$surv
+
+    S_tau <- cumhaz(
+      T_model,
+      newdata = data,
+      times = tau,
+      individual_time = individual_time
+    )$surv
+    S_tau <- as.vector(S_tau)
+
+    if (individual_time == FALSE) {
+      res <- 1 / S
+      res <- apply(res, 2, function(x) x * S_tau)
+      indicator_1 <- (u <= tau)
+      indicator_2 <- (u > tau)
+      res <- apply(res, 1, function(x) x * indicator_1 + indicator_2)
+      res <- t(res)
+    } else {
+      res <- S_tau / S * (u <= tau) + (u > tau)
+      res <- as.vector(res)
+    }
+
+    return(res)
+  }
+  return(H)
+}
+
+## ## Hu: H_\tau(u|X_i, A_i) = E[\min(T, \tau) | T_i \geq u, X_i, A_i] =
+## ## u + \frac{1}{S(u|X,A)} \int_u^\tau S(t|X,A) dt
+H_constructor_rmst <- function(T_model, time, event, tau,
+                               individual_time, sample = 0) {
+  force(T_model)
+  force(tau)
+  force(individual_time)
+  force(time)
+  force(event)
+  force(sample)
+
+  H <- function(u, data) {
+    S <- cumhaz(
+      T_model,
+      newdata = data,
+      times = u,
+      individual.time = individual_time
+    )$surv
+
+    tt <- time[event == 1]
+    if (sample > 0) {
+      tt <- subjumps(tt, size = sample, tau = tau)
+    }
+
+    S_T <- cumhaz(
+      T_model,
+      newdata = data,
+      times = tt,
+      individual.time = FALSE
+    )$surv
+    if (individual_time == FALSE) {
+      int_S <- apply(
+        S_T,
+        1,
+        function(x) {
+          int_surv(times = tt, surv = x,
+                   start = u, stop = tau, extend = FALSE)
+        },
+        simplify = FALSE
+      )
+      int_S <- do.call(what = "rbind", int_S)
+      res <- 1 / S
+      res <- res * int_S
+      res <- apply(res, 1, function(x) x + pmin(u, tau))
+      res <- t(res)
+    } else {
+      int_S <- numeric(length = length(u))
+      for (k in seq_along(u)) {
+        int_S[k] <- int_surv(
+          times = tt,
+          surv = S_T[k, ],
+          start = u[k],
+          stop = tau, extend = FALSE
+        )
+      }
+      res <- pmin(u, tau) + 1 / S * int_S
+    }
+    return(res)
+  }
+
+  return(H)
+}
