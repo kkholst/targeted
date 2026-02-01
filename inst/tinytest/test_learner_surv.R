24d2867ff18fbf58731d26756c0c4f2733123424
Klaus Holst <klaus@holst.it>
2026-02-01 22:56:52 +0100

    tests + roxy

--- inst/tinytest/test_learner_surv.R
+++ inst/tinytest/test_learner_surv.R
@@ -10,7 +10,7 @@ test_learner_surv_cox <- function() {
   m <- coxph(Surv(time, status>0) ~ age + strata(sex), data=sTRACE)
   expect_true(abs(coef(mod$fit)- coef(m)) < 1e-5)
 
-  pr1 <- mod$predict(data.frame(age=50, sex=0:1), time=10)
+  pr1 <- mod$predict(data.frame(age=50, sex=0:1), times=10)
   pr <- predict(
     m,
     newdata=data.frame(time=10,status=0, age=50, sex=0:1),
@@ -23,6 +23,7 @@ test_learner_surv_cox()
 test_learner_surv_rf <- function() {
   mod <- learner_surv_rf(Surv(time, status>0) ~ age + sex)
   mod$estimate(sTRACE)
-  pr1 <- mod$predict(data.frame(age=50, sex=0:1), time=10)
-  expect_true(length(pr1 == 2))
+  pr1 <- mod$predict(data.frame(age=50, sex=0:1), times=10)
+  expect_true(length(pr1) == 2)
 }
+test_learner_surv_rf()
