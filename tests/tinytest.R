c4c5b4aac702c23602e2cc0240554a9e76d084a5
Benedikt Sommer <benediktsommer92@gmail.com>
2026-05-27 12:15:58 +0200

    minor

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -2,6 +2,5 @@
 if (requireNamespace("tinytest", quietly = TRUE)) {
   options(Ncpus = 1)
   data.table::setDTthreads(1)
-  LOG_THRESHOLD <- logger::log_threshold()
   tinytest::test_package("targeted")
 }
