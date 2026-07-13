d6b94b50c8180a7d7350555cac089a228e4eadde
AndreasNordland <AndreasNordland@users.noreply.github.com>
2026-07-13 20:41:19 +0200

    Missing outcome imputation average treatment effect: moi() (#149)
    * init moi: mean missing outcome imputation
    
    * first implementation of moiate
    
    * slow test of moiate for binary outcomes
    
    * added test_moi() to slow test
    
    * Update test_moiate.R
    
    * added test_moiate_continuous() slow test
    
    * Update test_outcome_imputation.R
    
    * lints
    
    * changes propensity.model to treatment.model
    
    * Update test_moiate.R
    
    * moi update, propensity.model -> treatment.model
    
    * slow test
    
    Not super convincing - should be investigated closer
    
    * added imputation augmentation
    
    * Update moi.R
    
    * Update test_moiate.R
    
    * id ordering bug in moi
    
    * Update test_moiate.R
    
    * Update test_moiate.R
    
    * remove requirement of id in data
    
    * Update test_moiate.R
    
    * init degen test
    
    * update id edit
    
    * export moiate and added documentation
    
    * removed transform and back.transform
    
    * lint
    
    * delete example
    
    * Update moiate.Rd
    
    * Update test_moiate.R
    
    * update doc for moi
    
    * propensity.model -> treatment.model
    
    * update from dev
    
    * renamed moiate to moi
    
    * moi(): nfolds/folds shared between cate calls
    
    * moi(): use new lava syntax for est calculations
    
    * moi(): silent, stratify, mc.cores, second.order passed to cate()
    
    * moi() doc clean-up
    
    * moi(): aligned print output with cate()
    
    * utf8 bug fixed
    
    * moi_missing() extended.output argument
    
    * Update test_moi.R
    
    * moi_missing(): predict_glm(p, data)
    
    * create NA parameter bug test
    
    * Update DESCRIPTION
    
    * Now with handling of NA paramters in imputation.model
    
    fixed in lava
    
    * treatment.model now only allow a formula, e.g., a ~ 1
    
    * updating imputation pre-specified weights with respect to imputation subset
    
    * update roxygen2 to 8.0.0
    
    * lintr
    
    * Handling partially no missing outcome, partially missing outcomes, and all missing outcomes
    
    * augmented IC estimate using the augmented estimator
    
    * centralizing the augmented IC
    
    * Update moi.Rd
    
    * removed unused test
    
    * update doc
    
    * Update test_moi.R
    
    * predict_glm output now a vector
    
    * lintr
    
    * lava:::predict_glm and test bug
    
    * dependence: lava 1.9.2
    
    * update doc: added example
    
    * bug fix (delta_response) + formula imputation.model.augmentation
    
    * import predict_glm from lava
    
    * move slowtest/test_moi.R
    
    * \link & namespace (setNames)
    
    * roxygen
    
    * Update test_moi.R
    
    * minor tests
    
    * empty
    
    * missing outcome imputation improvements (#204)
    
    * handle user weights with specials
    
    ---------
    
    Co-authored-by: OANO (Andreas Nordland) <OANO@novonordisk.com>
    Co-authored-by: Klaus Holst <klaus@holst.it>
    Co-authored-by: Benedikt Sommer <benediktsommer92@gmail.com>
    Co-authored-by: Benedikt Sommer <55534750+benesom@users.noreply.github.com>

--- R/targeted-package.R
+++ R/targeted-package.R
@@ -6,7 +6,7 @@
 #' @import Rcpp methods
 #' @importFrom graphics plot points abline lines
 #' @importFrom grDevices nclass.Sturges
-#' @importFrom lava IC getoutcome estimate Inverse na.pass0 score logit expit parameter sim
+#' @importFrom lava IC getoutcome estimate Inverse na.pass0 score logit expit parameter sim predict_glm
 #' @importFrom stats approxfun as.formula update binomial deriv density glm.fit lm.wfit lm.fit glm lm coef vcov model.frame model.matrix na.pass nlminb predict dnorm quantile terms weighted.mean runif .getXlevels delete.response model.response gaussian formula model.offset reformulate drop.terms weights model.extract cov2cor pnorm pchisq uniroot sd na.omit offset
 #' @importFrom rlang hash call_match
 #' @importFrom R6 R6Class
